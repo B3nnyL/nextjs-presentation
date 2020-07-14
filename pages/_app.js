9b83c1a2f546eff6b40565034f8333753713f950
B3nnyL <shuyinglin47@gmail.com>
2020-07-14 18:02:30 +1000

    add example for metrics api

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import '../styles/global.css'
 
+export function reportWebVitals(metric) {
+  if (metric.label === 'web-vital') {
+    console.log(metric) // The metric object ({ id, name, startTime, value, label }) is logged to the console
+  }
+}
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
